c271f187df012dc59d2ff9d670a8007df4468eb5
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2025-11-24 17:46:11 +0100

    practice

--- find_matching_pattern.c
+++ find_matching_pattern.c
@@ -1,39 +1,38 @@
 #include "header.h"
 
 int find_matching_pattern(const char pattern[]) {
-	if (pattern == NULL) {
-		fprintf(stderr, "Error: Pattern is NULL\n");
-		return -1;
-	}
-	char line[MAX_LINE];
-	char matches[1000][MAX_LINE]; // Store up to 1000 matches
-	int match_found = 0;
+  if (pattern == NULL) {
+    fprintf(stderr, "Error: Pattern is NULL\n");
+    return -1;
+  }
+  char line[MAX_LINE];
+  char matches[1000][MAX_LINE];  // Store up to 1000 matches
+  int match_found = 0;
 
-	while (custom_getline(line, MAX_LINE) > 0) {
-		if (strindex(line, pattern) >= 0) {
-			strcpy(matches[match_found], line);
-			match_found++;
+  while (custom_getline(line, MAX_LINE) > 0) {
+    if (strindex(line, pattern) >= 0) {
+      strcpy(matches[match_found], line);
+      match_found++;
+    }
+  }
 
-		}
-	}
-
-	// After EOF/ CTRL+D, print all matching lines
-	if (match_found > 0) {
-		printf("\nMATCH:\n");
-		for (int i = 0; i < match_found; i++) {
-			printf("\033[4m%s\033[0m", matches[i]);
-			/* 
-			ANSI Escape Code for Underline
-			Start underline: \033[4m
-			End formatting: \033[0m
-			 */
-		}
-	} else {
-		printf("No matching lines found.\n");
-	}
-	return match_found;
+  // After EOF/ CTRL+D, print all matching lines
+  if (match_found > 0) {
+    printf("\nMATCH:\n");
+    for (int i = 0; i < match_found; i++) {
+      printf("\033[4m%s\033[0m", matches[i]);
+      /*
+      ANSI Escape Code for Underline
+      Start underline: \033[4m
+      End formatting: \033[0m
+       */
+    }
+  } else {
+    printf("No matching lines found.\n");
+  }
+  return match_found;
 }
-/* 
+/*
 int main(int argc, char *argv[])
 {
     const char *pattern = "default_pattern";
--- strindex.c
+++ strindex.c
@@ -2,14 +2,14 @@
 
 /* strindex: return index of t in s, -1 if none */
 int strindex(const char s[], const char t[]) {
-	int i, j, k;
+  int i, j, k;
 
-	for (i = 0; s[i] != '\0'; i++) {
-		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
-			;
-		if (k > 0 && t[k] == '\0') {
-			return i;		// Found a match, return starting index
-		}
-	}
-	return -1;		// No match found
+  for (i = 0; s[i] != '\0'; i++) {
+    for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+      ;
+    if (k > 0 && t[k] == '\0') {
+      return i;  // Found a match, return starting index
+    }
+  }
+  return -1;  // No match found
 }
